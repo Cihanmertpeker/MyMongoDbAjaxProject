f361a43e683796afcc45cd0cd676e3b6331abb6b
Cihan Mert PEKER <119748061+Cihanmertpeker@users.noreply.github.com>
2024-04-01 18:16:08 +0300

    Add project files.

---
+++ MyMongoDbAjaxProject/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Driver;
+using MyMongoDbAjaxProject.Dal.Entities;
+using MyMongoDbAjaxProject.Dal.Settings;
+
+namespace MyMongoDbAjaxProject.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly IMongoCollection<Category> _categoryCollection;
+
+        public CategoryController(IDatabaseSettings _databaseSettings)
+        {
+            var client = new MongoClient("DatabaseSettings");
+            var database = client.GetDatabase(_databaseSettings.DatabaseName);
+            _categoryCollection.Database.GetCollection<Category>(_databaseSettings.CategoryCollectionName);
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
